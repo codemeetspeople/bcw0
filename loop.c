2d466efaddb0a07390c6e542ca8af46b9a5a11db
Oleksandr Kunzietsov <caiman@7sky.dev>
2024-07-13 18:50:08 +0300

    commit

---
+++ loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int counter;
+
+    scanf("%d", &counter);
+
+    for ( ; counter > 0; counter-- ) {
+        printf("%d ", counter);
+    }
+    printf("0\n");
+
+    return 0;
+}
